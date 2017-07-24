3215317c6b00db888f095a6245d3847b4d5d2b78
François DEFEVER <fdefever.work@gmail.com>
2017-07-24 10:18:55 +0200

    Declare plugin and informations

---
+++ wp-content/plugins/post-it.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Post-it Plugin
+Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
+Description: Post-it is a simple visual plugin who give the possibility for an Author to activate a Checkbox with a message at the end of the post. The reader will see at the top of the page message from the Author.
+Version:     0.1
+Author:      François DEFEVER
+Author URI:  https://francoisdefever.com
+License:
+License URI:
+Text Domain:
+Domain Path:
+*/
